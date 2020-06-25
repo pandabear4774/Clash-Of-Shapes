bfbe16a7692c4e5e9ee160d169daff0cd68adb65
Panda Bear <65876658+pandabear4774@users.noreply.github.com>
2020-06-24 23:46:19 -0700

    Clash of Shapes V3
    Added Turret that defends player and is always active
    
    Rewrote some code to improve game performance
    
    Changed mouse spawning to only spawn on player side
    
    Fixed bugs on mouse click
    
    Added health to all troops

---
+++ Clash of Squares copy V3/Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float troopHealth;
+    public void DecreaseHealth(float damage)
+    {
+        troopHealth -= damage;
+        if (troopHealth <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+    }
+}
---
+++ Clash of Squares copy V3/Assets/Scripts/Wall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public float health;
+    public static float cost = 10;
+
+    public void DecreaseHealth(float damage)
+    {
+        if(health-damage <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        health -= damage;
+    }
+}
